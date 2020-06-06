03ed77e244761d0274fe879f016bd799c784405d
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-06-06 12:20:41 +0200

    Fixed some translations and handling game finished with a dialog

--- frontend/src/components/GameZone.vue
+++ frontend/src/components/GameZone.vue
@@ -8,6 +8,12 @@
       white_player_human="true"
       black_player_human="true"
       :reversed="board_reversed"
+
+      @checkmate="handleCheckmate($event)"
+      @stalemate="handleStalemate()"
+      @perpetual-draw="handlePerpetualDraw()"
+      @missing-material-draw="handleMissingMaterialDraw()"
+      @fifty-moves-draw="handleFiftyMovesDraw()"
     />
 
     <simple-modal-dialog
@@ -27,16 +33,19 @@
     >
       <v-card-text>{{$t('modals.stopGame.text')}}</v-card-text>
     </simple-modal-dialog>
+
+    <simple-snack-bar ref="snackBar" />
   </v-container>
 </template>
 
 <script>
 import SimpleModalDialog from './SimpleModalDialog';
+import SimpleSnackBar from './SimpleSnackBar';
 
 export default {
   data() {
     return {
-      board_reversed: false
+      board_reversed: false,
     };
   },
   methods: {
@@ -60,9 +69,32 @@ export default {
       const boardComponent = document.querySelector("loloof64-chessboard");
       boardComponent.stop();
     },
+    handleCheckmate(event) {
+      const whiteTurnBeforeMove = event.detail.whiteTurnBeforeMove;
+      const side = whiteTurnBeforeMove ? this.$i18n.t('gameFinished.whiteSide') : this.$i18n.t('gameFinished.blackSide');
+      const message = this.$i18n.t('gameFinished.checkmate', {side})
+      this.$refs["snackBar"].open(message);
+    },
+    handleStalemate() {
+      const message = this.$i18n.t('gameFinished.stalemate');
+      this.$refs['snackBar'].open(message);
+    },
+    handlePerpetualDraw() {
+      const message = this.$i18n.t('gameFinished.perpetualDraw');
+      this.$refs['snackBar'].open(message);
+    },
+    handleMissingMaterialDraw() {
+      const message = this.$i18n.t('gameFinished.missingMaterialDraw');
+      this.$refs['snackBar'].open(message);
+    },
+    handleFiftyMovesDraw() {
+      const message = this.$i18n.t('gameFinished.fiftyMovesDraw');
+      this.$refs['snackBar'].open(message);
+    }
   },
   components: {
     "simple-modal-dialog": SimpleModalDialog,
+    "simple-snack-bar": SimpleSnackBar,
   }
 };
 </script>
